9fbdd3b47e72f2b0e1bf955e56a5a90229cc8187
BiancaNL <bianca.vandepoel@gmail.com>
2021-05-15 10:23:12 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,27 @@
+<?php
+/**
+ * @package McLuhan Child Theme
+ */
 
+/**
+ * Load the stylesheets
+ */
+function mcluhan_custom_theme_style() {
+
+	// Parent theme styles
+	wp_enqueue_style( 'mcluhan-style', get_template_directory_uri(). '/style.css' );
+
+	// Child theme styles
+	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri(). '/style.css' );
+
+	// Custom font styles such as Inter font in this scenario
+   wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap', false );
+   
+}
+add_action( 'wp_enqueue_scripts', 'mcluhan_custom_theme_style' );
+
+
+/**
+ * Add logo support
+ */
+add_theme_support( 'custom-logo' );
